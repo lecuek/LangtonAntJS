4f99ee0acfbd26fb7f45ad7b5230bbb774142566
lecuek <lecuek@protonmail.com>
2022-12-07 21:09:25 +0100

    added style and sparkles

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 const GRID_DIV = document.getElementById("grid");
+const COUNTER = document.getElementById("counter");
 // const WIDTH = parseInt(prompt("Width? :"))
 // const HEIGHT = parseInt(prompt("Height? :"))
-const WIDTH = 500;
-const HEIGHT = 500;
-const CELL_HEIGHT = 5;
-const CELL_WIDTH = 5;
+const WIDTH = 550;
+const HEIGHT = 550;
+const CELL_HEIGHT = 2;
+const CELL_WIDTH = 2;
 const CELL_UNIT = "px";
 let globalGrid = [[], []];
-
+let totalSteps = 0;
 
 const COLORS = [
     "#FFFFFF",
-    "#0000FF",
-    "#00FF00",
-    "#FF0000"
+    "#000000",
+    "#111155",
+    "#AA1155"
 ]
 const COLORSMAP = {
     "#FFFFFF" : 0,
-    "#0000FF" : 1,
-    "#00FF00" : 2,
-    "#FF0000" : 3
+    "#000000" : 1,
+    "#111155" : 2,
+    "#AA1155" : 3
+
 }
 class Cell {
     constructor(element){
@@ -31,7 +33,9 @@ class Cell {
     nextColor(){
         this.color = COLORS[modulo((COLORSMAP[this.color]+1),COLORS.length)];
         this.element.style.backgroundColor = this.color;
+        this.element.classList.add("cellActive");
     }
+    
 }
 const DIRECTION = [
     0,
@@ -64,7 +68,7 @@ function modulo(a,n){
     return ((a % n ) + n ) % n
 }
 function initGrid(){
-
+    
     //initializes grid
     for (let i = 0; i < HEIGHT; i++) {
         let row = document.createElement("div");
@@ -73,9 +77,8 @@ function initGrid(){
         
         for (let j = 0; j < WIDTH; j++) {
             let cell = document.createElement("span");
-            cell.style.height = `${CELL_HEIGHT}${CELL_UNIT}`;
-            cell.style.width = `${CELL_WIDTH}${CELL_UNIT}`;
-            cell.style.backgroundColor = COLORS.white;
+            // cell.style.height = `${CELL_HEIGHT}${CELL_UNIT}`;
+            // cell.style.width = `${CELL_WIDTH}${CELL_UNIT}`;
             cell.classList.add("cell");
             row.appendChild(cell);
             globalGrid[i].push(new Cell(cell));
@@ -131,19 +134,32 @@ function step(times = 1) {
     
     moveAnt();
     cell.nextColor();
+    
     times -= 1;
+    totalSteps +=1;
     if(times > 0)
         step(times);
     
+    
+}
+function repeatSteps(){
+    step();
+    setTimeout(repeatSteps, 0);
 }
 
-function repeatStep(){
-    setTimeout(step, 100);
+function updateCounter(){
+    COUNTER.innerHTML = "Step: " + totalSteps;
+    window.setTimeout(updateCounter, 1000);
 }
 
-function main(){
+function main(speed = 1){
     Ant.position.x = Math.floor(WIDTH/2);
     Ant.position.y = Math.floor(HEIGHT/2);
+    GRID_DIV.innerHTML = "";
     initGrid();
-    repeatStep();
+    for (let i = 0; i < speed; i++) {
+        repeatSteps();
+    }
+    updateCounter();
+
 }
